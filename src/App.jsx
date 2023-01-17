a23f62f67e35aadb7faf44f79f7f06066e5f2989
Rodrigo <rodrigocantudo@gmail.com>
2023-01-17 17:59:01 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Info } from './info'
+
+function App() {
+
+
+  return (
+    <div className="App">
+
+<Info />
+    </div>
+  )
+}
+
+export default App
